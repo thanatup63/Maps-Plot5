e5f0b6f1ac8852d3b3ae7925a1039668cb9174bd
thanatup63 <63022924@up.ac.th>
2024-08-23 15:27:47 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
+  declarations: [AppComponent, GoogleMapComponent],
+  imports: [BrowserModule, FormsModule, GoogleMapsModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/google-map/google-map.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MapPolygon, MapInfoWindow, MapGeocoder } from '@angular/google-maps';
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.css']
+})
+export class GoogleMapComponent implements OnInit {
+  mapCenter = { lat: 13.7563, lng: 100.5018 };
+  zoom = 10;
+  polygonCoords: google.maps.LatLngLiteral[] = [];
+  polygonOptions: google.maps.PolygonOptions = {
+    editable: true,
+    draggable: true,
+    fillColor: 'blue',
+    fillOpacity: 0.3,
+    strokeColor: 'blue',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+  };
+  searchQuery: string = '';
+  lastClickedCoord: google.maps.LatLngLiteral | null = null;
+
+  constructor(private geocoder: MapGeocoder) {}
+
+  ngOnInit(): void {}
+
+  onPolygonEdit(event: google.maps.MapMouseEvent): void {
+    const path = event.latLng;
+    if (path) {
+      this.polygonCoords = [...this.polygonCoords, path.toJSON()];
+      this.lastClickedCoord = path.toJSON();
+    }
+  }
+
+  onClearPolygon(): void {
+    this.polygonCoords = [];
+    this.lastClickedCoord = null;
+  }
+
+  onSearchPlace(): void {
+    this.geocoder.geocode({ address: this.searchQuery }).subscribe((result) => {
+      if (result.results.length > 0) {
+        this.mapCenter = result.results[0].geometry.location.toJSON();
+        this.zoom = 15;
+      }
+    });
+  }
+}
